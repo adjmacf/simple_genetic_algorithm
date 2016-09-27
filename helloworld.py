a13863fc7ac416fb3f5ff4b8cfd435d3a402b396
alex <adjmacf@gmail.com>
2016-09-26 23:27:00 -0600

    added comments

--- helloworld.py
+++ helloworld.py
@@ -5,6 +5,10 @@ import time
 char_set = string.ascii_uppercase + string.ascii_lowercase + ' '
 
 def generate_organism(size):
+	"""
+	create a random list of characters at a certain size
+	Return the random list of characters
+	"""
 	organism = []
 	for i in range(size):
 		organism.append(char_set[random.randint(0, len(char_set) - 1)])
@@ -13,6 +17,10 @@ def generate_organism(size):
 	
 	
 def mutate(organism_in, size):
+	"""
+	randomly changes a random number of characters of an organism
+	Returns the mutateded organism or offspring	
+	"""
 	organism = organism_in[:]
 	number_mutations = int(random.randint(1, size-1))
 	
@@ -22,6 +30,10 @@ def mutate(organism_in, size):
 	return organism
 	
 def fitness(organism, target):
+	"""
+	evaluate how good an organism is
+	Returns number of matched characters
+	"""
 	score = 0
 	for i in range(len(target)):
 		if organism[i] == target[i]:
@@ -41,13 +53,15 @@ if __name__ == '__main__':
 	
 	generations = 0
 	
+	#repeat until we have the ideal organism/species
 	while best != target:
 	
-		for i in range(len(organisms)):
+		for i in range(len(organisms)):									#a little bit of fixing up here
 			if fitness(organisms[i], target) > best_fit_score:
 				best_fit_score = fitness(organisms[i], target)
 				best = organisms[i]
 		
+		#kill off last generation and produce offspring
 		for i in range(len(organisms)):
 			organisms[i] = mutate(best, target_len)		
 		
